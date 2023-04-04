6a0a4b6e6185c7c5d35bac88a3dfc069297a53fb
Matija Kočevar <matija.kocev@gmail.com>
2023-04-04 22:57:51 +0200

    skills fixed responsive

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
-import {
-  faTable,
-  faPaintBrush,
-  faDesktop,
-  faFileCode,
-} from '@fortawesome/free-solid-svg-icons';
+import { faTable, faPaintBrush, faDesktop, faFileCode } from '@fortawesome/free-solid-svg-icons';
 import './styles/skills.scss';
+import { Container, Row } from 'react-bootstrap';
 
 const Service = () => {
   return (
-    <div id='skills' className='skills'>
-      <h1 className='heading'>Skills</h1>
-      <div className='container'>
+    <Container id='skills' className='skills'>
+      <Row className='heading-row'>
+        <h1>Skills</h1>
+      </Row>
+      <Row className='skills-container'>
         <div className='box'>
           <div className='circle'>
             <FontAwesomeIcon className='icon' icon={faDesktop} size='2x' />
@@ -35,10 +33,7 @@ const Service = () => {
             <FontAwesomeIcon className='icon' icon={faTable} size='2x' />
           </div>
           <h3>Data visualising</h3>
-          <p>
-            Advanced knowledge of Microsoft Excel and a good grasp of how to
-            display important data.
-          </p>
+          <p>Advanced knowledge of Microsoft Excel and a good grasp of how to display important data.</p>
         </div>
         {/* - */}
         <div className='box'>
@@ -48,8 +43,8 @@ const Service = () => {
           <h3>Web development</h3>
           <p>Basic HTML, CSS, Javascript, C#, .NET core</p>
         </div>
-      </div>
-    </div>
+      </Row>
+    </Container>
   );
 };
 
