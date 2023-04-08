a4c38b4df95e097a10eb1659ac3af6b7f06b7336
Matija Kočevar <matija.kocev@gmail.com>
2023-04-08 21:59:22 +0200

    timelines responsive fix

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -8,35 +8,51 @@ const Education = () => {
         <h1>education</h1>
       </Row>
       <Row className='timeline-wrapper'>
-        <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Electrotechnician of computer science</h3>
-            <h5>2004 - 2008 / High school (.V)</h5>
-            <p>Gained basic programming skills in C++, HTML, CSS. </p>
-          </div>
-        </Col>
-        <Col xs={12} sm={12} md={12} lg={6}></Col>
         {/* - */}
-        <Col xs={12} sm={12} md={12} lg={6}></Col>
-        <Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Computer science and information technologies</h3>
-            <h5>2008 - 2011 / College (.VI) - unfinished</h5>
-            <p>Gained knowledge of Linux Shell, and furthered skills in C++, HTML and CSS.</p>
-          </div>
-        </Col>
+        <Row>
+          <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Electrotechnician of computer science</h3>
+              <h5>2004 - 2008 / High school (.V)</h5>
+              <p>Gained basic programming skills in C++, HTML, CSS. </p>
+            </div>
+          </Col>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+        </Row>
         {/* - */}
-        <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Learning front-end & back-end development</h3>
-            <h5>2020 - 2021 / Personal</h5>
-            <p>HTML, CSS, SQL, React JS, Vue JS, Node JS, .NET core, C#</p>
-          </div>
-        </Col>
-        <Col xs={12} sm={12} md={12} lg={6}></Col>
+        <Row>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+          <Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Computer science and information technologies</h3>
+              <h5>2008 - 2011 / College (.VI) - unfinished</h5>
+              <p>Gained knowledge of Linux Shell, and furthered skills in C++, HTML and CSS.</p>
+            </div>
+          </Col>
+        </Row>
+        {/* - */}
+        <Row>
+          <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Learning front-end & back-end development</h3>
+              <h5>2020 - 2021 / Personal</h5>
+              <p>HTML, CSS, SQL, React JS, Vue JS, Node JS, .NET core, C#</p>
+            </div>
+          </Col>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+        </Row>
       </Row>
     </Container>
   );
--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,59 +1,79 @@
-import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import './styles/experience.scss';
-import { Container, Row, Col } from 'react-bootstrap';
 
-const Experience = () => {
+const Education = () => {
   return (
     <Container id='timeline' className='experience'>
       <Row className='heading-row'>
         <h1>experience</h1>
       </Row>
       <Row className='timeline-wrapper'>
-        <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Data visualisation in Microsoft Excel and Google Sheets</h3>
-            <h5>2019 - 2020 / Mimovrste d.o.o.</h5>
-            <p>
-              Created semi-automated Excel documents for sales & stock overview connected with internal ERP system. Developed an automated system of
-              price checking using Google App Scripts & Sheets.
-            </p>
-          </div>
-        </Col>
-        <Col xs={0} sm={0} md={0} lg={6}></Col>
         {/* - */}
-        <Col xs={0} sm={0} md={0} lg={6}></Col>
-        <Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Created first API</h3>
-            <h5>2021 / Personal</h5>
-            <p>Developed with .NET core & Vue JS</p>
-          </div>
-        </Col>
+        <Row>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+          <Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Data visualisation in Microsoft Excel and Google Sheets</h3>
+              <h5>2019 - 2020 / Mimovrste d.o.o.</h5>
+              <p>
+                Created semi-automated Excel documents for sales & stock overview connected with internal ERP system. Developed an automated system of
+                price checking using Google App Scripts & Sheets.
+              </p>
+            </div>
+          </Col>
+        </Row>
         {/* - */}
-        <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Created first webpage</h3>
-            <h5>2021 / Personal</h5>
-            <p>Developed with React JS & Bootstrap 4.</p>
-          </div>
-        </Col>
-        <Col xs={0} sm={0} md={0} lg={6}></Col>
+        <Row>
+          <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Created first API</h3>
+              <h5>2021 / Personal</h5>
+              <p>Developed with .NET core & Vue JS</p>
+            </div>
+          </Col>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+        </Row>
         {/* - */}
-        <Col xs={0} sm={0} md={0} lg={6}></Col>
-        <Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>Software developer - Agitavit Solutions d.o.o.</h3>
-            <h5>2021 - current / Employment</h5>
-            <p>ReactTS, .NET 6, Umbraco, Sql server, jQuery</p>
-          </div>
-        </Col>
+        <Row>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+          <Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Created first webpage</h3>
+              <h5>2021 / Personal</h5>
+              <p>Developed with React JS & Bootstrap 4.</p>
+            </div>
+          </Col>
+        </Row>
+        {/* - */}
+        <Row>
+          <Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
+            <div className='line-wrapper'>
+              <div className='line'></div>
+              <div className='marker'></div>
+            </div>
+            <div className='timeline-content'>
+              <h3>Software developer - Agitavit Solutions d.o.o.</h3>
+              <h5>2021 - current / Employment</h5>
+              <p>ReactTS, .NET 6, Umbraco, Sql server, jQuery</p>
+            </div>
+          </Col>
+          <Col xs={0} sm={0} md={0} lg={6}></Col>
+        </Row>
       </Row>
     </Container>
   );
 };
 
-export default Experience;
+export default Education;
