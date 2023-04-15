ed5868b9aef646a935e972aa4ab362c501943c4e
Matija Kočevar <matija.kocev@gmail.com>
2023-04-16 00:35:37 +0200

    interests boostrap fix

--- src/components/Interests/interests.tsx
+++ src/components/Interests/interests.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import InterestsCarousel from './interestsCarousel';
 import './interests.scss';
 import { TranslationProps } from '../Types/types';
+import { Container, Row } from 'react-bootstrap';
 
 const Interests = (props: TranslationProps) => {
   return (
-    <div id='interests' className='interests'>
-      <h1>{props.getTranslation('Navigation_Interests')}</h1>
-      <div className='interests-content'>
-        <InterestsCarousel />
-      </div>
-    </div>
+    <Container id='interests' className='interests'>
+      <Row className='heading-row'>
+        <h1>{props.getTranslation('Navigation_Interests')}</h1>
+      </Row>
+      <Row>
+        <div className='interests-content'>
+          <InterestsCarousel />
+        </div>
+      </Row>
+    </Container>
   );
 };
 
