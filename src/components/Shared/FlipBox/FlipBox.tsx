f8c463a2756dae42d0a16d107f41713bf2b299de
Matija Kočevar <matija.kocev@gmail.com>
2023-05-01 16:05:29 +0200

    icons added, skill cards fixed

--- src/components/Shared/FlipBox/FlipBox.tsx
+++ src/components/Shared/FlipBox/FlipBox.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import './FlipBox.scss';
+import React, { useState } from "react";
+import "./FlipBox.scss";
 
 interface BoxProps {
-  frontContent: React.ReactNode;
-  backContent: React.ReactNode;
+	frontContent: React.ReactNode;
+	backContent: React.ReactNode;
 }
 
 const Box: React.FC<BoxProps> = ({ frontContent, backContent }) => {
-  const [flipped, setFlipped] = useState(false);
+	const [flipped, setFlipped] = useState(false);
 
-  const handleBoxClick = () => {
-    setFlipped(!flipped);
-  };
+	const handleBoxClick = () => {
+		setFlipped(!flipped);
+	};
 
-  return (
-    <div className={`flip-box ${flipped ? 'flipped' : ''}`} onClick={handleBoxClick}>
-      <div className='front'>{frontContent}</div>
-      <div className='back'>{backContent}</div>
-    </div>
-  );
+	return (
+		<div className={`flip-box ${flipped ? "flipped" : ""}`} onClick={handleBoxClick}>
+			<div className='front'>{frontContent}</div>
+			<div className='back'>{backContent}</div>
+		</div>
+	);
 };
 
 export default Box;
