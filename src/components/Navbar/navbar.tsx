7213597cebfee9cd66c391980360e90ff5fe8e20
Matija Kočevar <matija.kocev@gmail.com>
2023-10-08 23:19:29 +0200

    resume links changed

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -18,7 +18,7 @@ const NavBar = () => {
 	const hamburgerRef = useRef<HTMLButtonElement>(null);
 
 	const cvPath =
-		language === "slo" ? "https://drive.google.com/file/d/1a1YCu9V-mWrr4SYO-Ht4qirNyxsWM3Yh/view?usp=sharing" : "https://drive.google.com/file/d/1uYA8R8k4v2fiEu0N-WPjc8vbpFOojJHL/view?usp=sharing";
+		language === "slo" ? "https://drive.google.com/file/d/1DRTAJS8W0rNHXGcDIrruD7Fan5prkl-f/view?usp=sharing" : "https://drive.google.com/file/d/1DSA_eZccjlCkFxqDKgbq51MU7A50QhHP/view?usp=sharing";
 
 	const onLanguageChange = (language: string) => {
 		setLanguage(language);
