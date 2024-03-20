d4b6c69f0c7fccc788a0397366542f206d8755ce
Matija Kočevar <matija.kocev@gmail.com>
2024-03-20 19:34:07 +0100

    timeline fixes

--- src/components/Experience/experience.tsx
+++ src/components/Experience/experience.tsx
@@ -14,47 +14,36 @@ const Education = () => {
 			<Row className='timeline-wrapper'>
 				{/* - */}
 				<Row className='experience-section'>
-					<Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
+					<Col className='timeline-block' xs={0} sm={0} md={0} lg={6}></Col>
+					<Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
 						<div className='line-wrapper'>
 							<div className='line'></div>
 							<div className='marker'></div>
 						</div>
 						<div className='timeline-content'>{getTranslation("Experience_Mimovrste")}</div>
 					</Col>
-					<Col xs={0} sm={0} md={0} lg={6}></Col>
 				</Row>
 				{/* - */}
 				<Row className='experience-section'>
-					<Col xs={0} sm={0} md={0} lg={6}></Col>
-					<Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
+					<Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
 						<div className='line-wrapper'>
 							<div className='line'></div>
 							<div className='marker'></div>
 						</div>
 						<div className='timeline-content'>{getTranslation("Education_Personal")}</div>
 					</Col>
+					<Col className='timeline-block' xs={0} sm={0} md={0} lg={6}></Col>
 				</Row>
 				{/* - */}
 				<Row className='experience-section'>
-					<Col className='timeline-block timeline-block-left' xs={12} sm={12} md={12} lg={6}>
+					<Col className='timeline-block' xs={0} sm={0} md={0} lg={6}></Col>
+					<Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
 						<div className='line-wrapper'>
 							<div className='line'></div>
 							<div className='marker'></div>
 						</div>
 						<div className='timeline-content'>{getTranslation("Experience_Agitavit")}</div>
 					</Col>
-					<Col xs={0} sm={0} md={0} lg={6}></Col>
-				</Row>
-				{/* - */}
-				<Row className='experience-section'>
-					<Col xs={0} sm={0} md={0} lg={6}></Col>
-					<Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
-						<div className='line-wrapper'>
-							<div className='line'></div>
-							<div className='marker'></div>
-						</div>
-						<div className='timeline-content'>{getTranslation("Experience_Dhimahi")}</div>
-					</Col>
 				</Row>
 				{/* - */}
 				<Row className='experience-section'>
@@ -63,9 +52,20 @@ const Education = () => {
 							<div className='line'></div>
 							<div className='marker'></div>
 						</div>
+						<div className='timeline-content'>{getTranslation("Experience_Dhimahi")}</div>
+					</Col>
+					<Col className='timeline-block' xs={0} sm={0} md={0} lg={6}></Col>
+				</Row>
+				{/* - */}
+				<Row className='experience-section'>
+					<Col className='timeline-block' xs={0} sm={0} md={0} lg={6}></Col>
+					<Col className='timeline-block timeline-block-right' xs={12} sm={12} md={12} lg={6}>
+						<div className='line-wrapper'>
+							<div className='line'></div>
+							<div className='marker'></div>
+						</div>
 						<div className='timeline-content'>{getTranslation("Experience_Frodx")}</div>
 					</Col>
-					<Col xs={0} sm={0} md={0} lg={6}></Col>
 				</Row>
 			</Row>
 		</Container>
